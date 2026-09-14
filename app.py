288429d9472f7121f6a287ef37d975ae215ed90c
Aniruddh10-star <rathodanirudh949@gmail.com>
2026-09-14 22:50:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -680,8 +680,7 @@ def add_certificate():
     connection = get_connection()
     cursor = connection.cursor()
 
-        try:
-
+    try:
         placeholder = "%s" if DATABASE_URL else "?"
 
         cursor.execute(
